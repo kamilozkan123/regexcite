19aa12af77794a7a9740ce165936e6fff7eba215
kamilozkan123 <kamilozkan.ko@gmail.com>
2025-03-12 11:01:09 +0100

    Creating R documentation file for strsplit1() using roxygen2 and document()

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,14 @@
+#' Split a string
+#'
+#' @param x A charcter vector with one element
+#' @param split What to split on
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravocharlie,delta"
+#' strsplit1(x, ",")
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
